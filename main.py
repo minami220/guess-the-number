2d9e1a986692d6b5037977764ed50734daa51523
minami220 <regarlulun@gmail.com>
2025-05-31 22:44:27 +0700

    check validate user input

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-
+import random
+secret_guess = random.randint(1, 100)
 
 if __name__ =="__main__":
-
     print("WELCOME ON GUESS THE NUMBER")
     print("select a number between 1 and 100")
  
+    answer = input("Masukan Tebakanmu: ")
+
+    if answer.isdigit():
+        answer = int(answer)
+        print("answer is digit")
+        if answer < secret_guess:
+            print("Too Low")
+
+        elif answer > secret_guess:
+            print("Too High")
+        
+    print(answer)
