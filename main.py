2c5d37c13a54b2c09b44d2767a28878d519a572f
minami220 <regarlulun@gmail.com>
2025-06-01 18:17:22 +0700

    make the application to statefull

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 import random
-secret_guess = random.randint(1, 100)
+
+def main():
+    print("========================")
+    print("====GUESS THE NUMBER====")
+    print("========================")
+
+    low_num = 1
+    high_num = 100
+    print(f"Select a number between {low_num} and {high_num}")
+
+    attempt = 0
+    secret_guess = random.randint(1, 100)
+    is_running = True
+
+    while is_running:
+        answer = input("Masukan Tebakanmu:")
+        
+        if not answer.isdigit():
+            print(f"Please input the valid number between ({low_num},{high_num})")
+            continue
+            
+        answer = int(answer)
+        attempt += 1
+
+        if answer < secret_guess:
+            print("Too Low, Try Again")
+            continue
+
+        if answer > secret_guess:
+            print("Too High, Try Again")
+            continue
+
+        print(f"Correct, the number is {secret_guess}")
+        print("Attempt: ", attempt)
+        is_running = False
+
+
+
 
 if __name__ =="__main__":
-    print("WELCOME ON GUESS THE NUMBER")
-    print("select a number between 1 and 100")
- 
-    answer = input("Masukan Tebakanmu: ")
+    main()
 
-    if answer.isdigit():
-        answer = int(answer)
-        print("answer is digit")
-        if answer < secret_guess:
-            print("Too Low")
 
-        elif answer > secret_guess:
-            print("Too High")
-        
-    print(answer)
